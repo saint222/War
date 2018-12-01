1be5b126e6fe91a05e68ecbbc6a0641ada8d8bf0
saint12maloj <saint12maloj@gmail.com>
2018-12-02 01:09:56 +0300

    Начал писать Баттл

--- War/Battle.cs
+++ War/Battle.cs
@@ -8,5 +8,12 @@ namespace War
 {
     public class Battle
     {
+        Group russia_1 = new Group() { Name = "russia_1", Squads = new List<Squad> ()};
+        Group russia_2 = new Group() { Name = "russia_2", Squads = new List<Squad> ()};
+
+        Group usa_1 = new Group() { Name = "usa_1", Squads = new List<Squad> ()};
+        Group usa_2 = new Group() { Name = "usa_2", Squads = new List<Squad> ()};
+
+
     }
 }
